7d044d3fa7a1aef469e399327eab736eb3c70058
Zach Fudge <zacheryfudge@gmail.com>
2018-01-29 15:52:21 -0700

    added button box shadows. key input working for numbers, operators, and equals method

--- calc.js
+++ calc.js
@@ -3,14 +3,16 @@ const calc = {
 	mainDisplay: document.getElementById('main-display'),
 	subDisplay: document.getElementById('sub-display'),
 	equals: function() {
-		let n = calc.input.join('').split(/[^0-9\.]/)
-		if (n.length > 2) n.shift(),n[0] = n[0]*-1;
-		let o = calc.input.join('').split(/[0-9\.]/).join('');
-		if (o.length > 1) o = o[o.length -1];
-		const result = this.operators[o](parseInt(n[0]),parseInt(n[1]));
+		if (!this.isFloat()) {
+			let n = calc.input.join('').split(/[^0-9\.]/)
+			if (n.length > 2) n.shift(),n[0] = n[0]*-1;
+			let o = calc.input.join('').split(/[0-9\.]/).join('');
+			if (o.length > 1) o = o[o.length -1];
+			const result = this.operators[o](parseInt(n[0]),parseInt(n[1]));
 
-		this.mainDisplay.innerHTML = result;
-		this.input = [result];
+			this.mainDisplay.innerHTML = result;
+			this.input = [result];
+		}
 	},
 	clear: function() {
 		this.input = [];
@@ -19,10 +21,12 @@ const calc = {
 		this.subDisplay.innerHTML = "";
 	},
 	inputNumber: function(n) {
-		n = parseInt(n);
-		(this.input.length === 0 || this.isOperator(this.subDisplay.innerHTML[this.subDisplay.innerHTML.length-1])) ? this.mainDisplay.innerHTML = n : this.mainDisplay.innerHTML += n;
-		this.subAdd(n);
-		this.input.push(n);
+		if (this.mainDisplay.innerHTML.length < 12 || this.isOperator(this.input[this.input.length-1])) {
+			n = parseInt(n);
+			(this.input.length === 0 || this.isOperator(this.subDisplay.innerHTML[this.subDisplay.innerHTML.length-1])) ? this.mainDisplay.innerHTML = n : this.mainDisplay.innerHTML += n;
+			this.subAdd(n);
+			this.input.push(n);
+		}
 	},
 	operators: {
 		"*": (x,y) => x * y,
@@ -80,9 +84,35 @@ const calc = {
 			}
 		},
 		sqroot: function(n) {
-			calc.subDisplay.innerHTML = '&radic;' + "(" + calc.subDisplay.innerHTML + ")" 
-			return n ** 0.5;
+			if (calc.input.length > 0) {
+				if (calc.isFloat()) {
+					calc.subDisplay.innerHTML = "&radic;(" + calc.subDisplay.innerHTML + ")";
+					const result = Math.sqrt(parseFloat(calc.input.join("")));
+					calc.mainDisplay.innerHTML = result;
+					calc.input = [result];
+				} else {
+					calc.equals();
+					calc.x.sqroot();
+				}
+			}
 		} 
 	}
 };
 
+window.addEventListener("keydown", function(btn) { 
+	if (btn.keyCode === 13) calc.equals();
+	if (btn.keyCode === 48 || btn.keyCode === 96) calc.inputNumber(0);
+	if (btn.keyCode === 49 || btn.keyCode === 97) calc.inputNumber(1);
+	if (btn.keyCode === 50 || btn.keyCode === 98) calc.inputNumber(2);
+	if (btn.keyCode === 51 || btn.keyCode === 99) calc.inputNumber(3);
+	if (btn.keyCode === 52 || btn.keyCode === 100) calc.inputNumber(4);
+	if (btn.keyCode === 53 || btn.keyCode === 101) calc.inputNumber(5);
+	if (btn.keyCode === 54 || btn.keyCode === 102) calc.inputNumber(6);
+	if (btn.keyCode === 55 || btn.keyCode === 103) calc.inputNumber(7);
+	if (btn.keyCode === 56 || btn.keyCode === 104) calc.inputNumber(8);
+	if (btn.keyCode === 57 || btn.keyCode === 105) calc.inputNumber(9);
+	if (btn.keyCode === 107) calc.inputOperator("+"); 
+	if (btn.keyCode === 109) calc.inputOperator("-"); 
+	if (btn.keyCode === 106) calc.inputOperator("*"); 
+	if (btn.keyCode === 111) calc.inputOperator("/"); 
+});
