7a4132074c33d316a55908d250234c7c25b99800
Zach Fudge <zacheryfudge@gmail.com>
2018-01-29 11:31:18 -0700

    single operations working

--- calc.js
+++ calc.js
@@ -1,27 +1,51 @@
 const calc = {
+	input: [],
 	mainDisplay: document.getElementById('main-display'),
 	subDisplay: document.getElementById('sub-display'),
 	equals: function() {
+		const n = calc.input.join('').split(/[^0-9\.]/);
+		const o = calc.input.join('').split(/[0-9\.]/).join('');
 
+		const result = this.operators[o](parseInt(n[0]),parseInt(n[1]));
+
+		this.mainDisplay.innerHTML = result;
+		this.input = [result];
 	},
 	clear: function() {
-		calc.val
+		this.input = [];
+
+		this.mainDisplay.innerHTML = 0;
+		this.subDisplay.innerHTML = "";
+	},
+	inputNumber: function(n) {
+		n = parseInt(n);
+		
+		(this.input.length === 0 || isNaN(this.subDisplay.innerHTML[this.subDisplay.innerHTML.length-1]) && this.subDisplay.innerHTML[this.subDisplay.innerHTML.length-1] !== ".") ? this.mainDisplay.innerHTML = n : this.mainDisplay.innerHTML += n; 
+		this.subAdd(n);
+		this.input.push(n);
+	},
+	operators: {
+		"*": (x,y) => x * y,
+		"/": (x,y) => x / y,
+		"+": (x,y) => x + y,
+		"-": (x,y) => x - y
+	},
+	inputOperator: function(n, t) {
+		(this.isFloat()) ? this.input.push(n) : this.equals();
+		this.subAdd(n, t || n);
+	},
+	subAdd: function(c,t) {
+		this.subDisplay.innerHTML += `${t || c}`;
+	},
+	isFloat: function() {
+		return this.input.every((cur)=>!isNaN(cur) || cur === '.');
+	},
+	inputFloat: function(val = '.') {
+		this.subAdd(val);
+		this.input.push(val);
+		this.mainDisplay.innerHTML += val;
 	}
 };
 
-function inputNumber(n) {
-	n = parseInt(n);
-	console.log(typeof n);
-}
-
-function inputOperator(n) {
-	console.log(typeof n);
-	console.log(n);
-}
-
-const operators = {
-	"*": (x,y) => x * y,
-	"/": (x,y) => x / y,
-	"+": (x,y) => x + y,
-	"-": (x,y) => x - y
-};
+calc.input.join('').split(/[0-9\.]/).join('');
+calc.input.join('').split(/[^0-9\.]/);
